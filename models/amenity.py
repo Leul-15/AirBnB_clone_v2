65971abe21fa9c98dfbf3ae20c73504edb7e2c8b
Leul <leulassefa255@gmail.com>
2023-12-20 09:45:47 -0800

    AirBnB clone  MySQL

--- models/amenity.py
+++ models/amenity.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-"""Implementation of the Amenity class
-"""
-from os import getenv
-from models.base_model import BaseModel, Base
-from models.place import place_amenity
+""" State Module for HBNB project """
+from models.base_model import BaseModel
+from models.base_model import Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """Implementation for the Amenities.
+    """Amenity
     """
     __tablename__ = "amenities"
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
-        place_amenities = relationship("Place", secondary=place_amenity,
-                                       back_populates="amenities")
-    else:
-        name = ""
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place", secondary="place_amenity",
+                                   viewonly=False)
