a4fc744220e8516c3b9aa84d511ebd9fd1d46e14
Leul <leulassefa255@gmail.com>
2023-12-20 10:10:04 -0800

    AirBnB clone  MySQL

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -23,15 +23,13 @@ class DBStorage:
     __session = None
     def __init__(self):
         """Create engine and link to MySQL databse"""
-        user = getenv("HBNB_MYSQL_USER")
-        password = getenv("HBNB_MYSQL_PWD")
-        host = getenv("HBNB_MYSQL_HOST", default="localhost")
-        database = getenv("HBNB_MYSQL_DB")
-        env = getenv("HBNB_ENV")
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}:3306/{}'
-                                      .format(user, password, host, database),
-                                      pool_pre_ping=True)
-        if env == "test":
+        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".
+                                      format(getenv("HBNB_MYSQL_USER"),
+                                             getenv("HBNB_MYSQL_PWD"),
+                                             getenv("HBNB_MYSQL_HOST"),
+                                             getenv("HBNB_MYSQL_DB"),
+                                             pool_pre_ping=True))
+        if getenv("HBNB_ENV") == "test":
             Base.metadata.drop_all(self.__engine)
     
     def all(self, cls=None):
