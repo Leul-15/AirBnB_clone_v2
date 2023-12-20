2664fa5442755a9a2442d21aa891a4cf9bc562b9
Leul <leulassefa255@gmail.com>
2023-12-20 10:23:43 -0800

    AirBnB clone  MySQL

--- models/state.py
+++ models/state.py
@@ -13,8 +13,7 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
+    cities = relationship("City", backref="state", cascade="delete")
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
