17216e4a62638edce137c7fb3df83b2aa35ea323
Leul <leulassefa255@gmail.com>
2024-01-26 12:54:23 -0800

    AirBnB clone  Web framework

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -11,46 +11,6 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def home():
-    return "Hello HBNB!"
-
-
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    return "HBNB"
-
-
-@app.route("/c/<text>", strict_slashes=False)
-def c_text(text):
-    return "C " + text.replace("_", " ")
-
-
-@app.route("/python", strict_slashes=False)
-def python():
-    return "Python is cool"
-
-
-@app.route("/python/<text>", strict_slashes=False)
-def Python_text(text):
-    return "Python " + text.replace("_", " ")
-
-
-@app.route("/number/<int:n>", strict_slashes=False)
-def n_number(n):
-    return "{:d} is a number".format(n)
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    return render_template('5-number.html', num=n)
-
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n):
-    return render_template('6-number_odd_or_even.html', num=n)
-
-
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     return render_template('7-states_list.html',
