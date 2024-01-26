e1d1069d4c9c3fef9b0592e3a94bc926fd1c3b21
Leul <leulassefa255@gmail.com>
2024-01-26 11:49:15 -0800

    AirBnB clone  Web framework

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -69,3 +69,7 @@ class FileStorage:
             pass
         except KeyError:
             pass
+
+    def close(self):
+        """call reload() method for deserializing the JSON file to objects"""
+        self.reload()
