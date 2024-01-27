783a8b5b587abc6412ee55e625b1123b7fa4aae3
Leul <leulassefa255@gmail.com>
2024-01-27 06:53:13 -0800

    AirBnB clone  Web framework

---
+++ web_flask/9-states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+app = Flask(__name__)
+
+
+@app.route("/states")
+def states():
+    """
+    Retrieve all states from storage and render them in the template
+    """
+    states = storage.all("State").values()
+    return render_template("9-states.html", state=states)
+
+
+@app.route("/states/<id>")
+def states_id(id):
+    """
+    Retrieve a state by its ID and render the corresponding template
+    """
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
+
+
+@app.teardown_appcontext
+def teardown(err):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
